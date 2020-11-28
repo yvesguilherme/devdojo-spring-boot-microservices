01b7d97ac00d3359ec3323e5a083dd36b885bfb9
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2020-11-27 22:30:25 -0300

    :tada:  Setup Inicial

--- src/main/java/academy/devdojo/cursoyoutube/course/model/AbstractEntity.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.cursoyoutube.course.model;
-
-import java.io.Serializable;
-
-public interface AbstractEntity extends Serializable {
-    Long getId();
-}
--- src/main/java/academy/devdojo/cursoyoutube/course/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.cursoyoutube.course.repository;
-
-import academy.devdojo.cursoyoutube.course.model.Course;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
-}
